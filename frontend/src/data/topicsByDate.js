0ceaba9106fc7880fa864eba1d61ed562bbf61a7
kimssirr <kimssirr@naver.com>
2025-07-01 11:52:30 +0900

    fix: topic 날짜 수정

--- frontend/src/data/topicsByDate.js
+++ frontend/src/data/topicsByDate.js
@@ -1,58 +1,57 @@
 const topicsByDate = {
-  '2025-05-23': {
+  '2025-07-01': {
     topics: ['바다', '산'],
     translated: ['ocean', 'mountains']
   },
-  '2025-05-24': {
+  '2025-07-02': {
     topics: ['돈', '시간'],
     translated: ['money', 'time']
   },
-  '2025-05-25': {
+  '2025-07-03': {
     topics: ['사이다', '콜라'],
-    translated: ['soda', 'cola'] // 'soda can' 등으로 구체화하면 더 잘 나옴
+    translated: ['soda', 'cola']
   },
-  '2025-05-26': {
+  '2025-07-04': {
     topics: ['아이폰', '갤럭시'],
     translated: ['iPhone', 'Samsung phone']
   },
-  '2025-05-27': {
+  '2025-07-05': {
     topics: ['찍어 먹기', '부어 먹기'],
     translated: ['dipping sauce', 'pouring sauce']
   },
-  '2025-05-28': {
+  '2025-07-06': {
     topics: ['소주', '맥주'],
     translated: ['soju', 'beer']
   },
-  '2025-05-29': {
+  '2025-07-07': {
     topics: ['미래로 가기', '과거로 가기'],
-    translated: ['future city', 'retro style'] // 이미지 검색 의도에 맞게 보완
+    translated: ['future city', 'retro style']
   },
-  '2025-05-30': {
+  '2025-07-08': {
     topics: ['사자', '호랑이'],
     translated: ['lion', 'tiger']
   },
-  '2025-05-31': {
+  '2025-07-09': {
     topics: ['고양이', '강아지'],
     translated: ['cat', 'puppy'] 
   },
-  '2025-06-01': {
+  '2025-07-10': {
     topics: ['여름', '겨울'],
     translated: ['summer', 'winter']
   },
-  '2025-06-02': {
+  '2025-07-11': {
     topics: ['단팥호빵', '야채호빵'],
     translated: ['steamed bun', 'vegetable bun']
   },
-  '2025-06-03': {
+  '2025-07-12': {
     topics: ['팥 붕어빵', '슈크림 붕어빵'],
     translated: ['fish-shaped pastry', 'custard pastry'] 
   },
-  '2025-06-04': {
+  '2025-07-13': {
     topics: ['딱딱 복숭아', '물렁 복숭아'],
     translated: ['crunchy peach', 'soft peach']
   },
 };
-
 
 
 export default topicsByDate;
